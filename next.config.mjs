d8ced9c2be579857147f4e8a4f02be99a416a98b
illastatnik56-ship-it <illastatnik56@gmail.com>
2026-05-23 17:24:25 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  poweredByHeader: false,
+  experimental: {
+    typedRoutes: true,
+  },
+}
 
 export default nextConfig
